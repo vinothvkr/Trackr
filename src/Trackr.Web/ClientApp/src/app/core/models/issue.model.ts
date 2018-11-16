de21bea0f49862294e1036cf27c896ead51464f2
vinothvkr <vinothvkr@hotmail.com>
2018-11-16 17:34:27 +0530

    new issue domain model in clientapp

---
+++ src/Trackr.Web/ClientApp/src/app/core/models/issue.model.ts
@@ -0,0 +1,5 @@
+export class Issue {
+  id: number;
+  title: string;
+  body: string;
+}
--- src/Trackr.Web/ClientApp/src/app/core/models/project.model.ts
+++ src/Trackr.Web/ClientApp/src/app/core/models/project.model.ts
@@ -1,4 +1,8 @@
+import {Issue} from '../models/issue.model'
+
 export class Project {
   id: number;
   name: string;
+  description: string;
+  issues: Issue;
 }
