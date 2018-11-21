637c7f354847f53dea6fa35783c0e2c39877e250
vinothvkr <vinothvkr@hotmail.com>
2018-11-21 22:31:33 +0530

    add client login route

--- src/Trackr.Web/ClientApp/src/app/app-routing.module.ts
+++ src/Trackr.Web/ClientApp/src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { RouterModule, Routes, Route } from '@angular/router';
 import { ProjectsComponent } from './project/project.component';
 import { ProjectDetailComponent } from './project/project-detail.component';
 import { IssueDetailComponent } from './issue/issue-detail.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'projects/:id', component: ProjectDetailComponent },
   { path: 'projects/:projectId/issues/:id', component: IssueDetailComponent }
