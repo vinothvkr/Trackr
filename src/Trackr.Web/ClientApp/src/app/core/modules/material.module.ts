320ca437b3210bda70f7fb2a6483d00509d771d5
vinothvkr <vinothvkr@hotmail.com>
2018-11-22 22:02:52 +0530

    Add Mat modules

--- src/Trackr.Web/ClientApp/src/app/core/modules/material.module.ts
+++ src/Trackr.Web/ClientApp/src/app/core/modules/material.module.ts
@@ -12,7 +12,8 @@ import {
   MatTableModule,
   MatGridListModule,
   MatFormFieldModule,
-  MatInputModule
+  MatInputModule,
+  MatProgressSpinnerModule
 } from '@angular/material';
 
 @NgModule({
@@ -28,7 +29,8 @@ import {
     MatTableModule,
     MatGridListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     MatButtonModule,
@@ -42,7 +44,8 @@ import {
     MatTableModule,
     MatGridListModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatProgressSpinnerModule
   ]
 })
 export class MaterialModule { }
