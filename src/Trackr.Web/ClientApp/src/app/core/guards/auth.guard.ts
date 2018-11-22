8a6132180836bd5d0c81a666a0822ca97c478b2e
vinothvkr <vinothvkr@hotmail.com>
2018-11-22 22:01:46 +0530

    Add AuthGaurd

---
+++ src/Trackr.Web/ClientApp/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
