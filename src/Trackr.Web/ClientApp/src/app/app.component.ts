c8d42b340a589a65b3cec8f63a97ae0b71643ee4
vinothvkr <vinothvkr@hotmail.com>
2018-11-27 15:42:10 +0530

    Move sidenav to right side and add back button to left side in all the page except homepage

--- src/Trackr.Web/ClientApp/src/app/app.component.ts
+++ src/Trackr.Web/ClientApp/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { User } from './core/models/user.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,22 @@ import { User } from './core/models/user.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  currentUrl;
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private location: Location
+  ) {
+    router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        this.currentUrl = e.url;
+      }
+    });
+  }
+
+  goBack() {
+    this.location.back();
+  }
 
   logout() {
     this.authService.logout();
