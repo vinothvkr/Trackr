26d9317fae788f6f727c9c69291032062aad95d9
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:41:45 +0530

    Add IssueDto

---
+++ src/Trackr.Infrastructure/Dtos/Issue/IssueDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trackr.Infrastructure.Dtos.Issue
+{
+    public class IssueDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IssueTypeId { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
