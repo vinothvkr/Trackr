5d889cb9fabe825ccbe87fa850a4983fd562fca0
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:39:59 +0530

    Add ClaimsPrincipleExtension to get UserId and email

---
+++ src/Trackr.Infrastructure/Helpers/ClaimPrincipalExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Trackr.Infrastructure.Helpers
+{
+    public static class ClaimPrincipalExtension
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.Name)?.Value;
+        }
+    }
+}
