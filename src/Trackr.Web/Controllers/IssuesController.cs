bc2ee2664f829927781603b2456f7f67d9ee2f44
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:46:12 +0530

    IssueController Post method implementation

--- src/Trackr.Web/Controllers/IssuesController.cs
+++ src/Trackr.Web/Controllers/IssuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Trackr.Infrastructure.Dtos;
+using Trackr.Infrastructure.Dtos.Issue;
 using Trackr.Infrastructure.Models;
 using Trackr.Infrastructure.Services;
 
@@ -11,7 +13,7 @@ namespace Trackr.Web.Controllers
 {
     [Route("api/projects/{projectId}/issues")]
     [ApiController]
-    public class IssuesController : Controller
+    public class IssuesController : ControllerBase
     {
         private readonly IIssueService _issueService;
 
@@ -21,22 +23,26 @@ namespace Trackr.Web.Controllers
         }
         // GET: api/Issues
         [HttpGet]
-        public JsonResult Get(int projectId)
+        public IActionResult Get(int projectId)
         {
-            return Json(_issueService.GetAll(projectId));
+            return Ok(_issueService.GetAll(projectId));
         }
 
         // GET: api/Issues/5
         [HttpGet("{id}", Name = "GetIssue")]
-        public JsonResult Get(int projectId, int id)
+        public IActionResult Get(int projectId, int id)
         {
-            return Json(_issueService.Get(id));
+            return Ok(_issueService.Get(id));
         }
 
         // POST: api/Issues
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] IssueDto dto)
         {
+            return Ok(new ResultDto
+            {
+                Id = _issueService.Create(dto)
+            });
         }
 
         // PUT: api/Issues/5
