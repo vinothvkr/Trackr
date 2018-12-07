c467a5a7aa7fe3747e2636af39139c95c6d19266
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:53:05 +0530

    Get IssueTypes and NewIssue method implementation in Issue.Service

--- src/Trackr.Web/ClientApp/src/app/core/services/issue.service.ts
+++ src/Trackr.Web/ClientApp/src/app/core/services/issue.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Issue } from '../models/issue.model';
+import { IssueType } from '../models/issue-type.model';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,15 @@ export class IssueService {
   getIssue(projectid: number, id: number): Observable<Issue> {
     const url = `${this.issuesUrl}/${projectid}/issues/${id}`;
     return this.http.get<Issue>(url);
+  }
+
+  getIssueTypes(): Observable<IssueType[]> {
+    const url = 'api/issuetypes';
+    return this.http.get<IssueType[]>(url);
+  }
+
+  newIssue(projectId: number, issue: Issue): Observable<Issue> {
+    const url = `${this.issuesUrl}/${projectId}/issues`;
+    return this.http.post<Issue>(url, issue);
   }
 }
