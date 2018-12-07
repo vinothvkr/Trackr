683ae81e6cdf697860240a47d9fc48e5f4034b78
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:41:54 +0530

    Add ResultDto

---
+++ src/Trackr.Infrastructure/Dtos/ResultDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trackr.Infrastructure.Dtos
+{
+    public class ResultDto
+    {
+        public int Id { get; set; }
+    }
+}
