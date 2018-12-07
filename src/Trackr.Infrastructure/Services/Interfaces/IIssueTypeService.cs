db59873001979f2e4fb9f5e40540f7ab76645317
vinothvkr <vinothvkr@hotmail.com>
2018-12-07 21:42:56 +0530

    Add IssueTypeService

---
+++ src/Trackr.Infrastructure/Services/Interfaces/IIssueTypeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trackr.Infrastructure.Models;
+
+namespace Trackr.Infrastructure.Services
+{
+    public interface IIssueTypeService : IService<IssueType>
+    {
+    }
+}
---
+++ src/Trackr.Infrastructure/Services/IssueTypeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trackr.Infrastructure.Data;
+using Trackr.Infrastructure.Models;
+
+namespace Trackr.Infrastructure.Services
+{
+    public class IssueTypeService : Service<IssueType>, IIssueTypeService
+    {
+        public IssueTypeService(TrackrDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
