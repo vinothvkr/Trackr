5eb60ac2ba2d0a05d757a671c872099cd94d1844
vinothvkr <vinothvkr@hotmail.com>
2018-12-13 15:10:13 +0530

    Handle 500 error and show snackbar

--- src/Trackr.Web/ClientApp/src/app/core/helpers/error.interceptor.ts
+++ src/Trackr.Web/ClientApp/src/app/core/helpers/error.interceptor.ts
@@ -3,17 +3,24 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 import { AuthService } from '../services/auth.service';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if (err.status === 401) {
         this.authService.logout();
         location.reload(true);
+      }
+      if(err.status === 500) {
+        this.snackBar.open('Something went wrong.', '', { duration: 10000 });
       }
       const error = err.error.message || err.statusText;
       return throwError(error);
