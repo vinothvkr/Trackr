fc16c245ed7fde55e4d639a41f308e85aaf2bcf1
vinothvkr <vinothvkr@hotmail.com>
2018-12-13 22:41:10 +0530

    Add Comment Controller and Post action

---
+++ src/Trackr.Web/Controllers/CommentsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trackr.Infrastructure.Dtos.Comment;
+using Trackr.Infrastructure.Services;
+
+namespace Trackr.Web.Controllers
+{
+    [Route("api/projects/{projectId}/issues/{issueId}/comments")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentsController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        // GET: api/Comments
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Comments/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Comments
+        [HttpPost]
+        public IActionResult Post(int issueId, [FromBody] CommentDto comment)
+        {
+            comment.IssueId = issueId;
+            return Ok(_commentService.Create(comment));
+        }
+
+        // PUT: api/Comments/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
