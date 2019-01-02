94e42dada2347b574a78c1255e7e41441e79abfe
vinothvkr <vinothvkr@hotmail.com>
2019-01-02 17:50:55 +0530

    Add BaseController and Authorize attribute using Bearer scheme

---
+++ src/Trackr.Web/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trackr.Web.Controllers
+{
+    [Authorize(AuthenticationSchemes = IdentityServerAuthenticationDefaults.AuthenticationScheme)]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
