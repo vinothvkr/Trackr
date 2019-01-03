8d9a5f2de80dc07eabee324a26afb59b76b68ff8
vinothvkr <vinothvkr@hotmail.com>
2019-01-03 16:54:04 +0530

    Move IdentityServer configuration from startup.cs

---
+++ src/Trackr.Infrastructure/Configurations/IdentityServerConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trackr.Infrastructure.IdentityServer;
+
+namespace Trackr.Infrastructure.Configurations
+{
+    public class IdentityServerConfiguration
+    {
+        public static void ConfigurationService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentityServer()
+            .AddInMemoryClients(Clients.Get())
+            .AddInMemoryApiResources(Resources.GetApiResources())
+            .AddTestUsers(Users.Get())
+            .AddDeveloperSigningCredential();
+        }
+    }
+}
