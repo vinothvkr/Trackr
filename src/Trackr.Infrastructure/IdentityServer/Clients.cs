9954d8047b335c530c14ff243533f50f1a123f5e
vinothvkr <vinothvkr@hotmail.com>
2019-01-03 16:55:25 +0530

    Add IdentityServer configs

---
+++ src/Trackr.Infrastructure/IdentityServer/Clients.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trackr.Infrastructure.IdentityServer
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "TrackrWebClient",
+                    ClientName = "Trackr web client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("ClientSecret".Sha256())
+                    },
+                    AllowedScopes = new List<string> {"TrackrAPI.read"},
+                    AccessTokenLifetime = 60
+                }
+            };
+        }
+    }
+}
---
+++ src/Trackr.Infrastructure/IdentityServer/Resources.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trackr.Infrastructure.IdentityServer
+{
+    public class Resources
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource
+                {
+                    Name = "TrackrAPI",
+                    DisplayName = "Trackr API",
+                    Description = "Trackr API Access",
+                    UserClaims = new List<string> {"role"},
+                    ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                    Scopes = new List<Scope>
+                    {
+                        new Scope("TrackrAPI.read"),
+                        new Scope("TrackrAPI.write")
+                    }
+                }
+            };
+        }
+    }
+}
