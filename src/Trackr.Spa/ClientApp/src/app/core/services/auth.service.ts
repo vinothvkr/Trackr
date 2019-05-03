a3cbb5d89023a87bf785366d4390106dc5336479
vinothvkr <vinothvkr@hotmail.com>
2019-05-03 22:48:22 +0530

    Redirect to the previous page after login

--- src/Trackr.Spa/ClientApp/src/app/core/services/auth.service.ts
+++ src/Trackr.Spa/ClientApp/src/app/core/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { UserManager, UserManagerSettings, User, WebStorageStateStore } from 'oidc-client';
+import { Router } from '@angular/router'
 
-export const TOKEN_NAME: string = 'token';
+export const RETURN_URL: string = 'return_url';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class AuthService {
   private manager = new UserManager(getClientSettings());
   private user: User = null;
 
-  constructor() {
+  constructor(
+    private router: Router
+  ) {
     this.manager.getUser().then(user => {
       this.user = user;
     })
@@ -30,26 +33,27 @@ export class AuthService {
   }
 
   startAuthentication(): Promise<void> {
-    return this.manager.signinRedirect();
+    return this.manager.signinRedirect(window.localStorage.setItem(RETURN_URL, window.location.pathname));
   }
 
   completeAuthentication(): Promise<void> {
     return this.manager.signinRedirectCallback().then(user => {
       this.user = user;
+      this.router.navigate([window.localStorage.getItem(RETURN_URL)]);
     });
   }
 
-  logout() {
-    localStorage.removeItem(TOKEN_NAME);
+  logout(): Promise<void> {
+    return this.manager.signoutRedirect();
   }
 }
 
 export function getClientSettings(): UserManagerSettings {
   return {
-    authority: 'https://localhost:51864/',
+    authority: 'https://localhost:51864',
     client_id: 'TrackrWebClient',
     redirect_uri: 'https://localhost:51866/auth-callback',
-    post_logout_redirect_uri: 'https://localhost:51866/',
+    post_logout_redirect_uri: 'https://localhost:51866',
     response_type: "code",
     scope: "openid profile TrackrAPI",
     filterProtocolClaims: true,
