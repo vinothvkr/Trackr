8969bafbb7fa99cd65049c66d5820a82560adaac
vinothvkr <vinothvkr@hotmail.com>
2019-05-14 19:23:28 +0530

    Add AccountController

---
+++ src/Trackr.Api/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Trackr.Infrastructure.Dtos.Identity;
+using Trackr.Infrastructure.Services;
+
+namespace Trackr.Api.Controllers
+{
+    [Route("api/account/[action]")]
+    [ApiController]
+    public class AccountController : BaseController
+    {
+        private readonly IIdentityService _identityService;
+
+        public AccountController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupDto m)
+        {
+            var result = await _identityService.Create(m);
+
+            return Ok(result.Succeeded);
+        }
+    }
+}
