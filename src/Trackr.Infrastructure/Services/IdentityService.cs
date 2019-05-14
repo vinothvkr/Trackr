b0033d59183a321a8fb983bc20e3496dd525db63
vinothvkr <vinothvkr@hotmail.com>
2019-05-14 19:20:59 +0530

    Add IdentityService

---
+++ src/Trackr.Infrastructure/Services/IdentityService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trackr.Infrastructure.Data;
+using Trackr.Infrastructure.Dtos.Identity;
+using Trackr.Infrastructure.Models;
+
+namespace Trackr.Infrastructure.Services
+{
+    public class IdentityService : Service<ApplicationUser>, IIdentityService
+    {
+        public readonly TrackrDbContext _dbContext;
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public IdentityService(TrackrDbContext dbContext,
+            UserManager<ApplicationUser> userManager) : base(dbContext)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> Create(SignupDto m)
+        {
+            var user = new ApplicationUser
+            {
+                Email = m.Email,
+                UserName = m.Email
+            };
+            var result = await _userManager.CreateAsync(user, m.Password);
+            return result;
+        }
+    }
+}
---
+++ src/Trackr.Infrastructure/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Trackr.Infrastructure.Dtos.Identity;
+using Trackr.Infrastructure.Models;
+
+namespace Trackr.Infrastructure.Services
+{
+    public interface IIdentityService : IService<ApplicationUser>
+    {
+        Task<IdentityResult> Create(SignupDto m);
+    }
+}
